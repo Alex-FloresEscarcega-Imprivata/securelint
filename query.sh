1f5069f96978c4e61ec76a07b2595554546e213c
Alex Flores Escarcega <Alex.FloresEscarcega@IMPRIVATA.com>
2023-05-17 15:42:27 -0500

    Tweak query.sh to accept current working directory as first argument

--- query.sh
+++ query.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 DIR=$1
 shift
+
+
+if [ "$DIR" == "." ]; then
+  DIR=$(pwd)
+fi
+
 FILES="$@"
 
 docker run -it -v ${DIR}:${DIR} securelint:latest query --test 'rules/java-rules.txt' ${FILES}
