9107115d15f55b48f6f4fee3218c48dd6efa88b7
Alex Flores Escarcega <Alex.FloresEscarcega@IMPRIVATA.com>
2023-05-18 14:11:29 -0500

    basic git integration for all changes since master

--- query.sh
+++ query.sh
@@ -7,6 +7,25 @@ if [ "$DIR" == "." ]; then
   DIR=$(pwd)
 fi
 
-FILES="$@"
+FILES=""
 
+while getopts "g" opt; do
+  case ${opt} in
+    g )
+      FILES=$(git diff --name-only master | awk -v dir="$DIR" '{printf "\"%s/%s\" ", dir, $0}')
+      ;;
+    /? )
+      echo "Invalid option: $OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+echo $FILES
+shift $((OPTIND -1))
+
+if [ -z "$FILES" ]; then
+  FILES="$@"
+fi
+echo $FILES
+echo $DIR
 docker run -it -v ${DIR}:${DIR} securelint:latest query --test 'rules/java-rules.txt' ${FILES}
